4c7ba637482df2eb0941581eda23cfcc87c72a17
zeus <zeuskeyz@gmail.com>
2024-05-22 09:11:33 +0300

    added login validation code to the signIn api and added the preliminary task schema

--- database_files/schemas.js
+++ database_files/schemas.js
@@ -1,7 +1,24 @@
+//USER SCHEMA
 const user = {
-    username: { type: String, required: true, lowercase: true },
-    email: { type: String, required: true, lowercase: true },
-    password: { type: String, requiredd: true }
+
+    username: { type: String, required: true, lowercase: true, unique: true },
+    email: { type: String, required: true, lowercase: true, unique: true },
+    shop: { type: String, required: true, lowercase: true },
+    type: { type: String, required: true, lowercase: true },
+    password: { type: String, required: true }, 
+
+} 
+
+//TASKS SCHEMA
+const task = {
+
+    name: { type: String, required: true, lowercase: true },
+    summary: { type: String, required: true, lowercase: true },
+    status: { type: String, required: true, lowercase: true, default: 'open'},
+    shop: { type: String, required: true, lowercase: true },
+    creator: { type: String, required: true, lowercase: true },
+    handler: { type: String, default: '', lowercase: true },
+
 }
 
-module.exports = {user}
+module.exports = { user, task }
--- routes/user_routes.js
+++ routes/user_routes.js
@@ -20,17 +20,34 @@ const createUser = async (req, res) => {
             const hashedPass = await bcrypt.hash(req.body.password, 13) //HASH PASSWORD
             const newUser = new userModel({ ...req.body, password: hashedPass })//CREATE A NEW USER INCLUDING THE HASHED PASSWORD
             await newUser.save(); //SAVE USER TO DATABASE
-            res.send({ reply: 'user created successfully' })
+            res.send('user created successfully') //RESPONSE IF USER IS CREATED
 
-        } else res.send({ reply: 'please choose another username/email' })
+        } else res.send('please choose another username/email') //RESPONSE IF USER DETAILS ALREADY EXISTS
 
-    } catch (error) { res.send({ reply: error['message'] }) }
+    } catch (error) { res.send(error.message) } //FEEDBACK IF USER CREATION PROCESS FAILS TO START
 }
 
-
 //READS USERMODEL TO ALLOW LOGIN
-const signIn = (req,res) => {
-    res.send(req.session)
+const signIn = async (req,res) => {
+    try {
+
+        const exists = await userModel.findOne({username:req.body.username}) //CHECKS IF USER EXISTS IN DB
+
+        if (exists) {
+    
+            const loginAuth = await bcrypt.compare(req.body.password, exists.password) //VALIDATES USER PASSWORD
+    
+            if (loginAuth) {
+
+                delete exists._doc.password; //REMOVES THE PASSWORD FROM USER OBJECT
+                req.session.user = {...exists._doc, validated:true} //MODIFIES THE SESSION OBJECT
+                res.send(req.session)
+    
+            } else res.send('invalid password') //FEEDBACK IF PASSWORD IS INVALID
+            
+        } else { res.send('invalid username/email')} //FRESPONSE IF USERNAME IS INVALID
+        
+    } catch (error) { res.send(error.message) } //FEEDBACK IF USER LOGIN PROCESS FAILS TO START
 }
 
 
