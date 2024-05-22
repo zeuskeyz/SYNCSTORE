b36ad547c2adae5690e8bfbfcf50827ca7febe3c
zeus <zeuskeyz@gmail.com>
2024-05-22 20:29:38 +0300

    made changes to the task schema

--- database_files/schemas.js
+++ database_files/schemas.js
@@ -5,21 +5,25 @@ const user = {
     email: { type: String, required: true, lowercase: true, unique: true },
     shop: { type: String, required: true, uppercase: true },
     type: { type: String, required: true, lowercase: true },
-    squads: {type:Array, default:['shop' ]},
+    squads: { type: Array, default: ['shop'] },
     password: { type: String, required: true },
 
-} 
+}
 
 //TASKS SCHEMA
 const task = {
 
     name: { type: String, required: true, lowercase: true },
     summary: { type: String, required: true, lowercase: true },
-    status: { type: String, required: true, lowercase: true, default: 'open'},
+    status: { type: String, required: true, lowercase: true, default: 'open' },
     shop: { type: String, required: true, lowercase: true },
     creator: { type: String, required: true, lowercase: true },
-    handler: { type: String, default: '', lowercase: true },
+    audience: { type: String, default: '' }, //GROUP
+    assignee: { type: String, required: true }, //EVERYONE OR ANYONE
+    picklist: { type: Array, default: [] },
+    checkout: { type: Array, default: [] },
+    comments: { type: Array, default: [] },
 
 }
-
+   
 module.exports = { user, task }
